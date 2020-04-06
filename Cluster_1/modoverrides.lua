500770c3b39cbc4e28a64087d9e952e7342d5998
travis <tgaff@alumni.nd.edu>
2020-04-06 02:09:53 +0000

    fixed missing mod

--- Cluster_1/modoverrides.lua
+++ Cluster_1/modoverrides.lua
@@ -1,5 +1,6 @@
 return {
   ["workshop-569043634"]={ configuration_options={  }, enabled=true },
+  ["workshop-356420397"]={ configuration_options={  }, enabled=true },
   ["workshop-1788625832"]={ configuration_options={  }, enabled=true },
   ["workshop-347079953"]={
     configuration_options={ DFV_Language="EN", DFV_MinimalMode="default" },
