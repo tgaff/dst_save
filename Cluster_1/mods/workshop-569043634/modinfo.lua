62f3259bea595a424011d7fef2c0407c274d866b
travis <tgaff@alumni.nd.edu>
2020-04-11 04:38:53 +0000

    winter

---
+++ Cluster_1/mods/workshop-569043634/modinfo.lua
@@ -0,0 +1,20 @@
+name = "Campfire Respawn"
+description = "Allows you to respawn, when you haunt campfires.".."\n".."Works the same way as the Jury-Rigged Portal.".."\n\n".."Affects:".."\n".."*Campfire".."\n".."*Firepit".."\n".."*Endothermic Fire".."\n".."*Endothermic Fire Pit".."\n".."Only works in Endless and Survival mode.".."\n".."Version 1.3"
+author = "VampireMonkey"
+version = "v1.3"
+
+dont_starve_compatible = true
+dst_compatible = true
+reign_of_giants_compatible = true
+
+client_only_mod = false
+all_clients_require_mod = true
+
+forumthread = ""
+
+api_version = 10
+
+server_filter_tags = {"campfire respawn"}
+
+icon_atlas = "modicon.xml"
+icon = "modicon.tex"
