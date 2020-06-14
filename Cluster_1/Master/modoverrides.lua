43f56b97c81febaf4525e9533e737f05102c7b8c
travis <tgaff@alumni.nd.edu>
2020-06-14 04:22:58 +0000

    add bigger boats and craftable chesters

--- Cluster_1/Master/modoverrides.lua
+++ Cluster_1/Master/modoverrides.lua
@@ -28,5 +28,29 @@ return {
       Slots=80 
     },
     enabled=true 
+  },
+  ["workshop-1839858501"]={ configuration_options={  }, enabled=true },
+  ["workshop-862195447"]={
+    configuration_options={
+      CHESTER=0,
+      HUTCH=0,
+      MAIN=0,
+      PACKIM=0,
+      chester_craft="{Ingredient(\"livinglog\", 1),Ingredient(\"rope\", 1),Ingredient(\"goldnugget\", 1),Ingredient(\"nightmarefuel\", 1)}",
+      chester_locations="{}",
+      chester_max=1,
+      chester_science="{tech = TUNING.PROTOTYPER_TREES.SHADOWMANIPULATOR, hint = string.sub(STRINGS.UI.CRAFTING.NEEDSHADOWMANIPULATOR, 1, -14)..\"!\"}",
+      followertypes=7,
+      hutch_craft="{Ingredient(\"ice\", 1),Ingredient(\"waterballoon\", 4),Ingredient(\"fish\", 1),Ingredient(\"bluegem\", 1)}",
+      hutch_locations="{}",
+      hutch_max=1,
+      hutch_science="{tech = TUNING.PROTOTYPER_TREES.ANCIENTALTAR_HIGH}",
+      ownership=false,
+      packim_craft="{Ingredient(\"fish\", 1), Ingredient(\"nightmarefuel\", 8), Ingredient(\"Obsidian\", 4, \"images/ia_inventoryimages.xml\")}",
+      packim_locations="{}",
+      packim_max=1,
+      packim_science="{tech = TUNING.PROTOTYPER_TREES.SEALAB, hint = string.sub(STRINGS.UI.CRAFTING.NEEDSSEALAB, 1, -14)..\"!\"}"
+    },
+    enabled=true
   } 
 }
